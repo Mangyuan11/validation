4a1091cd102cd53d2a9df162714c0d3a2d12bc5f
Mangyuan11 <hoferyuanm@gmail.com>
2024-07-31 10:18:10 +0800

    Validation

---
+++ resources/views/user_tasks.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation From</title>
+</head>
+<body>
+    <form action="{{ url('user_tasks')}}" method="POST">
+        @csrf
+        <div>
+            <label for="task_name">Task Name</label>
+            <input type="text" id="name" name="task_name" value="{{ old('task_name')}}" />
+            @error('task_name')
+                <div>{{ $message }}</div> 
+            @enderror
+        </div>
+        <div>
+            <label for="status">Status</label>
+            <select id="status" name="status">
+                    <option value="">--</option>
+                    <option value="Pending">Pending</option>
+                    <option value="Completed">Completed</option>
+                    <option value="Cancelled">Cancelled</option>
+            @error('status')
+                <div>{{ $message }}</div> 
+            @enderror
+        </div>
+        <div>
+            <label for="description">Description:</label>
+            <textarea id="description" name="description"  rows="5" style="width: 100%"></textarea>
+            @error('description')
+                <div>{{ $message }}</div> 
+            @enderror
+        </div>
+        <div>
+            <label for="deadline">Deadline:</label>
+            <input type="date" id="deadline" name="deadline" value="{{ old('deadline')}}"></select>
+            @error('deadline')
+                <div>{{ $message }}</div> 
+            @enderror
+            </div>
+            <button type="submit">Submit</button>
+    </form>
+    @if (session('success'))
+        <div>{{ session('success')}}</div>
+    @endif
+</body>
+</html>
